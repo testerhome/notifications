fbdace743efae05814855c4504af1914d4ef6e70
chenhengjie <chenhengjie123@gmail.com>
2018-08-28 23:46:26 +0800

    fix: 修正 router 配置值

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Notifications::Engine.routes.draw do
   resources :notifications, path: "" do
     collection do
+
       delete '/:group', to: 'notifications#clean'
       get "/:group", to: 'notifications#group_by_type'
+
     end
   end
 end
