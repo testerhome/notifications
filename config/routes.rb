6bdf92446af97af1087816077cccb569dee3a927
chenhengjie <chenhengjie123@gmail.com>
2018-08-28 23:49:13 +0800

    fix: 修正语法错误

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Notifications::Engine.routes.draw do
   resources :notifications, path: "" do
     collection do
-
       delete '/:group', to: 'notifications#clean'
       get "/:group", to: 'notifications#group_by_type'
 
