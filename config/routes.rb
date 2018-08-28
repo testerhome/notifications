1b9e0569c64b293ec8beb44ee6925dd0936d414f
chenhengjie <chenhengjie123@gmail.com>
2018-08-29 00:00:31 +0800

    fix: 指定 group_by_type 方法路径

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Notifications::Engine.routes.draw do
     collection do
       delete '/:group', to: 'notifications#clean'
       get "/:group", to: 'notifications#group_by_type'
-
     end
   end
 end
