b806e95fa5588e632319fc58c5b4ce43bf416518
chenhengjie <chenhengjie123@gmail.com>
2018-08-29 00:12:12 +0800

    feat: 调整 clean 的路由规则

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Notifications::Engine.routes.draw do
   resources :notifications, path: '' do
     collection do
-      delete :clean
+      delete '/:group', to: 'notifications#clean'
       get "/:group", to: 'notifications#group_by_type'
     end
   end
